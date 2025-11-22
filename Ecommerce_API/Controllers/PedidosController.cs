2a65ee1be25bb73e894b69cb4dea110df1002fba
Ian <Ian@DESKTOP-U5COJFV>
2025-11-22 19:22:01 -0300

    feito o tratamento no pedido controller

--- Ecommerce_API/Controllers/PedidosController.cs
+++ Ecommerce_API/Controllers/PedidosController.cs
@@ -2,6 +2,7 @@
 using Application.DTOs;
 using Microsoft.AspNetCore.Mvc;
 using Ecommerce_API.Services;
+using Domain.Helpers;
 
 namespace Ecommerce_API.Controllers;
 [Route("api/[controller]")]
@@ -14,21 +15,58 @@ public class PedidosController : ControllerBase
     {
         _pedidosService = pedidosService;
     }
-    [HttpPost]
+    [HttpPost("incluir")]
     public ActionResult Incluir(PedidoDTO pedidoDTO)
     {
-        _pedidosService.Incluir(pedidoDTO);
-        return Ok();
+        try
+        {
+                _pedidosService.Incluir(pedidoDTO);
+            return Ok("O pedido foi incluido com sucesso.");
+
+        }
+        catch(DomainException ex)
+        {
+            return BadRequest(ex.Message);
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
     }
     [HttpGet]
     public ActionResult Listar()
     {
-        return Ok(_pedidosService.Listar());
+        try
+        {
+            var listaPedidos = _pedidosService.Listar();
+            return Ok(listaPedidos);
+
+        }
+        catch (DomainException ex)
+        {
+            return NotFound(ex.Message);
+        }
+        catch (Exception)
+        {
+            return StatusCode(500, "Erro interno do servidor.");
+        }
     }
     [HttpDelete("{id}")]
     public ActionResult Remover(int id)
     {
-        _pedidosService.Remover(id);
-        return Ok();
+        try
+        {
+            _pedidosService.Remover(id);
+            return Ok("O pedido foi removido com sucesso.");
+
+        }
+        catch (DomainException ex)
+        {
+            return NotFound(ex.Message);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, "Erro interno do servidor: " + ex.Message);
+        }
     }
 }
