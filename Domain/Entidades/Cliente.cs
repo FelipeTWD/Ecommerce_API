f7992bcb33f6537996372d09d0e1c687dc4500ed
Thauã <thauacerqueira07@gmail.com>
2025-11-29 20:27:31 -0300

    classe Cliente.cs senha e email privado

--- Domain/Entidades/Cliente.cs
+++ Domain/Entidades/Cliente.cs
@@ -4,8 +4,8 @@ public class Cliente
 {
     public int Id { get; set; }
     public string Nome { get; set; } = string.Empty;
-    public string Email { get; set; } = string.Empty;
-    public string Senha { get; set; } = string.Empty;
+    public string Email { get; private set; } = string.Empty;
+    public string Senha { get; private set; } = string.Empty;
     public Endereco EnderecoCliente { get; set; } = new();
     public  string Endereco { get; set; }
 }
